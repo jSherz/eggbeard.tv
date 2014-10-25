9939e63db646fba17c4c44627a045a2f337126ae
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-10-25 15:55:17 +0100

    Made the active_visit method create a visit if one doesn't exist.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
 
   def active_visit
-    visits.where(end: nil).first
+    visits.where(end: nil).first || Visit.create!(user: self, start: DateTime.now)
   end
 end
