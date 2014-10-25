198e0be5d1794b277d29d851e3851e392104b026
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-10-25 16:45:51 +0100

    Cleaned up spec.

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -10,82 +10,84 @@ RSpec.describe EggBot do
     @empty_message = mock_empty_message(@username)
   end
 
-  context 'when a user joins' do
-    it 'creates a User if required' do
-      users = User.where(:username, @username)
-      expect(users.length).to eq(0)
-
-      @plugin.user_joined(@empty_message)
+  context 'existing user' do
+    before :each do
+      @user = User.create!(username: @username)
 
       expect(User.first.username).to eq(@username)
     end
 
-    it "doesn't create a User if one exists" do
-      User.create!(username: @username)
+    context 'when a user joins' do
+      it "doesn't create a User if one exists" do
+        # Check no username taken exception thrown
+        expect { @plugin.user_joined(@empty_message) }.to_not raise_error
+      end
 
-      # The user exists
-      expect(User.first.username).to eq(@username)
+      it 'creates a Visit if required' do
+        @plugin.user_joined(@empty_message)
 
-      # The user joins the channel w/o throwing unique exception
-      @plugin.user_joined(@empty_message)
+        expect(@user.active_visit).to_not be_nil
+      end
+
+      it "doesn't create a Visit when one exists" do
+        visit = Visit.create!(user: @user, start: DateTime.now)
+
+        # Check we've created an active visit
+        expect(@user.active_visit).to_not be_nil
+        expect(@user.active_visit.end).to be_nil
+
+        @plugin.user_joined(@empty_message)
+
+        # Still the same last visit
+        expect(@user.active_visit).to eq(visit)
+      end
     end
 
-    it 'creates a Visit if required' do
-      user = User.create!(username: @username)
+    context 'when a user leaves' do
+      it 'uses the existing User if present' do
+        # The user exists
+        expect(User.first.username).to eq(@username)
 
-      @plugin.user_joined(@empty_message)
+        # The user leaves the channel w/o throwing unique exception
+        @plugin.user_parted(@empty_message)
+      end
 
-      expect(user.active_visit).to_not be_nil
-    end
+      it 'creates a Visit record if required' do
+        # The user exists
+        expect(User.first.username).to eq(@username)
 
-    it "doesn't create a Visit when one exists" do
-      user = User.create!(username: @username)
-      visit = Visit.create!(user: user, start: DateTime.now)
+        @plugin.user_parted(@empty_message)
 
-      # Check we've created an active visit
-      expect(user.active_visit).to_not be_nil
-      expect(user.active_visit.end).to be_nil
+        # It now has a closed visit
+        expect(@user.visits.last.end).to_not be_nil
 
-      @plugin.user_joined(@empty_message)
-
-      # Still the same last visit
-      expect(user.active_visit).to eq(visit)
+        # No active visits
+        expect(@user.visits.where(end: nil).length).to eq(0)
+      end
     end
   end
 
-  context 'when a user leaves' do
-    it 'creates the User if required' do
-      users = User.where(:username, @username)
-      expect(users.length).to eq(0)
+  context 'new user' do
+    context 'when a user joins' do
+      it 'creates a User if required' do
+        users = User.where(:username, @username)
+        expect(users.length).to eq(0)
 
-      @plugin.user_parted(@empty_message)
+        @plugin.user_joined(@empty_message)
 
-      expect(User.first.username).to eq(@username)
+        expect(User.first.username).to eq(@username)
+      end
     end
 
-    it 'uses the existing User if present' do
-      User.create!(username: @username)
+    context 'when a user leaves' do
+      it 'creates the User if required' do
+        users = User.where(:username, @username)
+        expect(users.length).to eq(0)
 
-      # The user exists
-      expect(User.first.username).to eq(@username)
+        @plugin.user_parted(@empty_message)
 
-      # The user leaves the channel w/o throwing unique exception
-      @plugin.user_parted(@empty_message)
-    end
-
-    it 'creates a Visit record if required' do
-      user = User.create!(username: @username)
-
-      # The user exists
-      expect(User.first.username).to eq(@username)
-
-      @plugin.user_parted(@empty_message)
-
-      # It now has a closed visit
-      expect(user.visits.last.end).to_not be_nil
-
-      # No active visits
-      expect(user.visits.where(end: nil).length).to eq(0)
+        expect(User.first.username).to eq(@username)
+      end
     end
   end
 end
