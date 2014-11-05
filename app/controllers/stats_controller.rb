c13032fc93c7547d2cae76dc1878a50b4e2a5ecb
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-11-05 19:20:28 +0000

    Added two more stats. Improved styling (slightly).

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,5 +3,7 @@ class StatsController < ApplicationController
   def index
     @first_visit = Visit.order(start: :desc).first
     @total_hours_watched = Visit.total_hours # To hours
+    @total_users = User.count
+    @total_visits = Visit.count
   end
 end
--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,2 +1,9 @@
 module StatsHelper
+  def format_stat(value)
+    number_with_precision(value, precision: 0, delimiter: ',')
+  end
+
+  def render_stat(value, &description)
+    render partial: 'stat_number', locals: { value: value, description: capture(&description) }
+  end
 end
