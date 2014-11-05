6b84421e6ea6f577cf69946db9a0a1bffbd11be4
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2014-11-05 18:43:20 +0000

    Started work on stats page.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,7 @@
 # Pages that display viewer stats
 class StatsController < ApplicationController
   def index
+    @first_visit = Visit.order(start: :desc).first
+    @total_hours_watched = Visit.total_hours # To hours
   end
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,4 +4,16 @@ class Visit < ActiveRecord::Base
 
   validates :user, presence: true
   validates :start, presence: true
+
+  def self.total_hours
+    total_secs / 3600
+  end
+
+  def self.total_secs
+    Visit.select(total_secs_query).order('').first.total_secs || 0
+  end
+
+  def self.total_secs_query
+    Arel.sql('EXTRACT(EPOCH FROM SUM(visits.end - visits.start))').as('total_secs')
+  end
 end
