3ddf4e17893dbe17069899203fec008ca03f8a9d
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2015-12-20 21:08:55 +0000

    Readded important logic.

---
+++ backend/user.rb
@@ -0,0 +1,22 @@
+# Represents a Twitch user
+class User < ActiveRecord::Base
+  has_many :visits
+
+  validates :username, presence: true
+  validates_uniqueness_of :username
+
+  def active_visit
+    visits.where(end: nil).first || Visit.create!(user: self, start: DateTime.now)
+  end
+
+  def end_active_visit(end_datetime = DateTime.now)
+    # Get first nil or highest end date
+    visit = active_visit
+    visit.end = end_datetime
+    visit.save
+  end
+
+  def self.ignored_users
+    %w(eggbeard nightbot autoegg)
+  end
+end
---
+++ bot/bot.rake
@@ -0,0 +1,19 @@
+require './bot/bot'
+
+namespace :bot do
+  task start: :environment do
+    bot = Cinch::Bot.new do
+      configure do |c|
+        c.server = Figaro.env.irc_host
+        c.port = Figaro.env.irc_port
+        c.nick = Figaro.env.irc_nick
+        c.password = Figaro.env.irc_password
+        c.channels = [Figaro.env.irc_channel]
+        c.plugins.plugins = [EggBot]
+      end
+    end
+
+    bot.start
+  end
+end
+
